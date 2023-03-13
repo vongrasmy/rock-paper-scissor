7e4aa35cde0d643b959f70bcf080ca9ffb2890d6
vongrasmy <svongrasmy95@gmail.com>
2023-03-13 07:51:45 -0500

    added round wins and draws and display message after first to 5 points

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let playerSelection;
-
+let playerWins = 0;
+let draw = 0;
+let computerWins = 0;
 function getComputerChoice() {
 
     return Math.floor(Math.random() * 3);
@@ -23,36 +25,36 @@ function playRound(computerSelection, playerSelection){
     }
 
     if (computerSelection == playerSelectionNum){
-        document.getElementById("round-result").innerHTML = "You both chose " + playerSelection + ". It's a draw!"
+        document.getElementById("round-result").innerHTML = "Results: You both chose " + playerSelection + ". It's a draw!"
         return draw+=1;
     } else if((computerSelection == 0) & (playerSelectionNum == 1)){
         playerWins+=1;
-        document.getElementById("round-result").innerHTML ="You chose " + playerSelection + ". Computer chose rock. You win! "
+        document.getElementById("round-result").innerHTML ="Results: You chose " + playerSelection + ". Computer chose rock. You win! "
         return playerWins;
 
     } else if((computerSelection == 1) & (playerSelectionNum == 0)){
         
-        document.getElementById("round-result").innerHTML ="You chose " + playerSelection + ". Computer chose paper. You lose!"
+        document.getElementById("round-result").innerHTML ="Results: You chose " + playerSelection + ". Computer chose paper. You lose!"
         return computerWins +=1;
 
     }else if((computerSelection == 2) & (playerSelectionNum == 1)){
         
-        document.getElementById("round-result").innerHTML ="You chose " + playerSelection + ". Computer chose scissor. You lose!"
+        document.getElementById("round-result").innerHTML ="Results: You chose " + playerSelection + ". Computer chose scissor. You lose!"
          return computerWins +=1;
 
     } else if((computerSelection == 1) & (playerSelectionNum == 2)){
          playerWins+=1;
-         document.getElementById("round-result").innerHTML = "You chose " + playerSelection + ". Computer chose paper. You win!"
+         document.getElementById("round-result").innerHTML = "Results: You chose " + playerSelection + ". Computer chose paper. You win!"
          return playerWins;
 
     }else if((computerSelection == 2) & (playerSelectionNum == 0)){
          playerWins+=1;
-         document.getElementById("round-result").innerHTML = "You chose " + playerSelection+ ". Computer chose scissor. You win!"
+         document.getElementById("round-result").innerHTML = "Results: You chose " + playerSelection+ ". Computer chose scissor. You win!"
          return playerWins;
 
     }else if((computerSelection == 0) & (playerSelectionNum == 2)){
          
-        document.getElementById("round-result").innerHTML = "You chose " + playerSelection + ". Computer chose rock. You lose!"
+        document.getElementById("round-result").innerHTML = "Results: You chose " + playerSelection + ". Computer chose rock. You lose!"
          return computerWins +=1;
 
     }
@@ -69,7 +71,24 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
         let computerSelection = getComputerChoice();
-        playRound(computerSelection,playerSelection)
+       let roundOutput = playRound(computerSelection,playerSelection)
+        console.log(roundOutput);
+        if( roundOutput== 1){
+            console.log(playerWins+=1);
+            document.getElementById("player-score").innerHTML = "Player Wins: " + playerWins;
+        }else if (roundOutput == 2){
+            console.log(computerWins +=1);
+            document.getElementById("computer-score").innerHTML = "Computer Wins: " + computerWins;
+        }else if (roundOutput == 3){
+            console.log(draw +=1);
+            document.getElementById("draws").innerHTML = "Draws: " + draw;
+        }
+
+        if(playerWins == 5){
+            document.getElementById("winner-announcement").innerHTML = "You won 5 rounds!";
+        } else if(computerWins == 5){
+            document.getElementById("winner-announcement").innerHTML = "The computer won 5 rounds!";
+        }
       });
     });
 
